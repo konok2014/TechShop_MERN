c713efca6d267f47f3ec143eb90c83381cb74045
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-01-20 02:54:50 +0600

    Serving & Fetching Data From Express with MongoDB

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+//@desc Fetch all product
+//@route GET/api/product
+//@access Public
+router.get('/',asyncHandler(async(req,res)=>{
+    const products=await Product.find({})
+    res.json(products)
+}))
+
+//@desc Fetch single product
+//@route GET/api/product/:id
+//@access Public
+
+// router.get('/:id',asyncHandler( async(req,res)=>{
+//     if(mongoose.Types.ObjectId.isValid(req.params.id)){
+//     const product= await Product.findById(req.params.id)
+//     if(product){
+//        res.json(product)
+//    } else{
+//         res.status(404).json({message:'Product not found'})
+//    }
+// }else{
+//     res.status(404).json({message:'Product not found'})
+// }
+// }))
+
+router.get('/:id',asyncHandler( async(req,res)=>{
+    const product= await Product.findById(req.params.id)
+    if(product){
+       res.json(product)
+    }else{
+        res.status(404)
+        throw new Error('Product not Found')
+   }
+ }
+))
+
+export default router
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import {Container,Row,Col} from "react-bootstrap"
 import Product from "../components/Product"
-import products from '../products'
+import axios from 'axios'
+
 const HomeScreen = () => {
+    const [product,setProduct]=useState([])
+    useEffect(() => {
+        const fetchProducts=async()=>{
+            const {data} =await axios.get('/api/products')
+
+            setProduct(data)
+        }
+        fetchProducts()
+    },[])
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
-            {products.map((product)=>(
+            {product.map((product)=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
