0afdc1000442ac7ad7a40d42ec54c4beef1166e8
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-01-22 03:53:33 +0600

    Implementing Redux For State Management

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,33 @@
 import React,{useEffect, useState} from 'react'
 import {Container,Row,Col} from "react-bootstrap"
 import Product from "../components/Product"
-import axios from 'axios'
+import {useDispatch,useSelector} from 'react-redux'
+import { listProducts } from '../actions/productsAction'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+
 
 const HomeScreen = () => {
-    const [product,setProduct]=useState([])
-    useEffect(() => {
-        const fetchProducts=async()=>{
-            const {data} =await axios.get('/api/products')
+   const dispatch=useDispatch()
 
-            setProduct(data)
-        }
-        fetchProducts()
-    },[])
+   const productList= useSelector(state => state.productList)
+   const {loading,error,products}=productList
+    useEffect(() => {
+       dispatch(listProducts())
+    },[dispatch])
+   
     return (
         <>
             <h1>Latest Products</h1>
+            {loading?<Loader/>:error?<Message/>:
             <Row>
-            {product.map((product)=>(
+            {products.map((product)=>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
             ))}
-            </Row>
+            </Row>}
+            
         </>
     )
 }
