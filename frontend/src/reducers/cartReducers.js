04669c7738f49d3739a5c6281bb153924c9b0b81
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-01-23 04:26:37 +0600

    Adding The Shopping Cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,7 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants"
 
 export const cartReducer=(state={cartItem:[]},action)=>{
-    switch (action.type){
+    switch (action.type) {
         case CART_ADD_ITEM:
             const item=action.payload
 
