c82db45821de648516d91d716255ed3b2fe7192a
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-01-29 20:23:33 +0600

    Back End User Athentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,12 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import { getProducts, getProductById } from '../controllers/productController.js'
 const router = express.Router()
-import Product from '../models/productModel.js'
 
-//@desc Fetch all product
-//@route GET/api/product
-//@access Public
-router.get('/',asyncHandler(async(req,res)=>{
-    const products=await Product.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
-//@desc Fetch single product
-//@route GET/api/product/:id
-//@access Public
-
+export default router
 // router.get('/:id',asyncHandler( async(req,res)=>{
 //     if(mongoose.Types.ObjectId.isValid(req.params.id)){
 //     const product= await Product.findById(req.params.id)
@@ -28,15 +20,14 @@ router.get('/',asyncHandler(async(req,res)=>{
 // }
 // }))
 
-router.get('/:id',asyncHandler( async(req,res)=>{
-    const product= await Product.findById(req.params.id)
-    if(product){
-       res.json(product)
-    }else{
-        res.status(404)
-        throw new Error('Product not Found')
-   }
- }
-))
+// router.get('/:id',asyncHandler( async(req,res)=>{
+//     const product= await Product.findById(req.params.id)
+//     if(product){
+//        res.json(product)
+//     }else{
+//         res.status(404)
+//         throw new Error('Product not Found')
+//    }
+//  }
+// ))
 
-export default router
