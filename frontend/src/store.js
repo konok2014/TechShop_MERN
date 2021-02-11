39306a7ad8d9076825464d393e70bea2071c5ff5
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-02-11 23:53:19 +0600

    Admin Screen access security & User Delete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productsReducer'
 import { cartReducer } from './reducers/cartReducers'
-import { userDetailsReducer, userListReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { userDeleteReducer, userDetailsReducer, userListReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducer'
 
 const reducer =combineReducers({
@@ -15,13 +15,11 @@ const reducer =combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile:userUpdateProfileReducer,
     userList:userListReducer,
+    userDelete:userDeleteReducer,
     orderCreate:orderCreateReducer,
     orderDetails:orderDetailsReducer,
     orderPay:orderPayReducer,
     orderListMy: orderListMyReducer,
-
-
-    
 })
 const cartItemFromStorage=localStorage.getItem('cartItem')
  ?JSON.parse(localStorage.getItem('cartItems'))
