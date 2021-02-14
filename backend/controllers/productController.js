80cd75e0f6f7124ba18024d32b3c1a6354c761be
konok2014 <64254730+konok2014@users.noreply.github.com>
2021-02-14 22:45:28 +0600

    Create review Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -85,4 +85,39 @@ const updateProduct=asyncHandler(async(req,res)=>{
 
 })
 
-export{getProducts,getProductById,deleteProduct,createProduct,updateProduct}
+//@desc Create a Product
+//@route PUT/api/products/:id/reviews
+//@access Private
+
+const createProductReview=asyncHandler(async(req,res)=>{
+  const {rating,comment}=req.body
+
+  const product = await Product.findById(req.params.id)
+  if(product){
+    const alreadyReviewed =product.review.find(r => r.user.toString()===req.user._id.toString())
+    
+   if(alreadyReviewed){
+    res.status(400)
+    throw new Error('Product already reviewed')
+  }
+  const review ={
+    name:req.user.name,
+    rating:Number(rating),
+    comment,
+    user:req.user._id
+
+  }
+  product.review.push(review)
+  product.numReview= product.review.length
+  product.rating = product.review.reduce((acc,item)=> item.rating + acc,0) / product.review.length
+  await product.save()
+  res.status(201).json({message:'Review added'})
+  }else{
+    res.status(404)
+    throw new Error('Product not Found')
+  }
+ 
+  
+})
+
+export{getProducts,getProductById,deleteProduct,createProduct,updateProduct,createProductReview}
